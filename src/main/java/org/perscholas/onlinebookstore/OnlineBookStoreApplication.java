e3e898bc950e4a1fe3b3f7520d0eed80a702a20d
Aruna <arunakay19@gmail.com>
2023-03-05 21:56:13 -0500

    Captone Online book store initial files.

---
+++ src/main/java/org/perscholas/onlinebookstore/OnlineBookStoreApplication.java
@@ -0,0 +1,16 @@
+package org.perscholas.onlinebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// @EnableEncryptableProperties
+public class OnlineBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookStoreApplication.class, args);
+	}
+
+
+
+}
